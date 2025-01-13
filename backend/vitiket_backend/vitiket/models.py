a01df5d1802b4a61cbe994524580be147207b79b
Grëg Häris <d.gregharis@gmail.com>
2025-01-13 13:00:00 +0100

    feat: create models

--- backend/vitiket_backend/vitiket/models.py
+++ backend/vitiket_backend/vitiket/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+  name = models.CharField(max_length=255)
+  date = models.DateField()
+  location = models.CharField(max_length=255)
+  available_tickets = models.IntegerField()
+  
+  def __str__(self):
+    return self.name
+  
+class Ticket(models.Model):
+  event = models.ForeignKey(Event, on_delete=models.CASCADE)
+  user_email = models.EmailField()
+  ticket_id = models.CharField(max_length=255, unique=True)
+  is_used = models.BooleanField(default=False)
+  
+  def __str__(self):
+    return f"Ticket {self.ticket_id} for {self.event.name}"
+  
+  
