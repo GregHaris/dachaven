ce622dc9947dde9c30beac7903eef7815172a498
Grëg Häris <d.gregharis@gmail.com>
2025-02-04 14:53:53 +0100

    feat: remove the outline and shadows of the input tags

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -287,7 +287,7 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
                       type="number"
                       placeholder="Price"
                       {...field}
-                      className="p-regular-16 border-0 bg-grey-50 outline-offset-0 focus:border-0 focus-visible:ring-0 focus-visible:ring-offset-0"
+                      className="p-regular-16 input-field"
                       onChange={(e) => {
                         field.onChange(e);
                         if (e.target.value) {
