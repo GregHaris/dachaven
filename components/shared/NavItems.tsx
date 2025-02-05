a2c036f8f6b0bd4d8017296f5a58290d4d8051ea
Grëg Häris <d.gregharis@gmail.com>
2025-02-05 03:34:01 +0100

    style: change text color and then add backgroung color on hover

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,24 +1,27 @@
-'use client'
+'use client';
 
 import { headerLinks } from '@/constants';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 
 const NavItems = () => {
-const pathName = usePathname()
+  const pathName = usePathname();
 
   return (
-    <ul className="md:flex-between flex w-full flex-col gap-5 items-start md:flex-row">
+    <ul className="md:flex-between flex w-full flex-col gap-5 md:gap-3 items-start md:flex-row text-grey-600 ">
       {headerLinks.map((link) => {
         const isActive = pathName === link.route;
         return (
-          <li key={link.route}
-          className={`${isActive && 'text-primary-500'} flex-center p-medium-16 whitespace-nowrap`}
+          <li
+            key={link.route}
+            className={`${
+              isActive && 'text-primary-500'
+            } flex-center p-medium-16 whitespace-nowrap hover:bg-grey-50 px-5 py-1 rounded-md`}
           >
             <Link href={link.route}>{link.label}</Link>
           </li>
-        )
-        })}
+        );
+      })}
     </ul>
   );
 };
