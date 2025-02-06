e4a1e10751e7a5562b725a0f2e169d8a3360dcc6
Grëg Häris <d.gregharis@gmail.com>
2025-02-06 19:56:34 +0100

    feat: add mobile hero images

--- constants/index.ts
+++ constants/index.ts
@@ -13,20 +13,7 @@ export const headerLinks = [
   },
 ];
 
-export const eventDefaultValues = {
-  title: '',
-  description: '',
-  location: '',
-  imageUrl: '',
-  startDateTime: new Date(),
-  endDateTime: new Date(),
-  categoryId: '',
-  price: '',
-  isFree: false,
-  url: '',
-};
-
-export const heroSlides = [
+export const desktopHeroSlides = [
   {
     image: '/assets/images/hero3.png',
   },
@@ -40,3 +27,40 @@ export const heroSlides = [
     image: '/assets/images/hero1.png',
   },
 ];
+
+export const mobileHeroSlides = [
+  {
+    image: '/assets/images/mobHero.png',
+  },
+  {
+    image: '/assets/images/4.png',
+  },
+  {
+    image: '/assets/images/5.png',
+  },
+  {
+    image: '/assets/images/7.png',
+  },
+  {
+    image: '/assets/images/8.png',
+  },
+  {
+    image: '/assets/images/9.png',
+  },
+  {
+    image: '/assets/images/10.png',
+  },
+];
+
+export const eventDefaultValues = {
+  title: '',
+  description: '',
+  location: '',
+  imageUrl: '',
+  startDateTime: new Date(),
+  endDateTime: new Date(),
+  categoryId: '',
+  price: '',
+  isFree: false,
+  url: '',
+};
