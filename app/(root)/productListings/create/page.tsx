0627edea5e76c502f2839da7ed2bec210e2429e2
Grëg Häris <d.gregharis@gmail.com>
2025-02-10 21:10:07 +0100

    feat: add heading texts

--- app/(root)/productListings/create/page.tsx
+++ app/(root)/productListings/create/page.tsx
@@ -1,22 +1,27 @@
-import { auth } from '@clerk/nextjs/server';
+'use client';
+
+import { useUser } from '@clerk/nextjs';
 import ProductListingsForm from '@/components/shared/ProductListingForm';
 
-const CreateProductListings = async () => {
-  const { sessionClaims } = await auth();
+const CreateProductListings = () => {
+  const { isLoaded, user } = useUser();
 
-  // Type assertion to help TypeScript understand the structure
-  const claims = sessionClaims as CustomJwtSessionClaims;
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
 
-  // Access userId from the nested object
-  const userId = claims?.userId?.userId as string;
+  const userId = user?.id as string;
+  const userName = user?.firstName || 'User';
 
   return (
     <>
-      {' '}
-      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
-        <h3 className="wrapper h3-bold text-center sm:text-left">
-          Create ProductListings
-        </h3>
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-1 md:py-5">
+        <div className="wrapper text-left">
+          <h3 className=" h3-bold">Hey {userName}!</h3>
+          <p className="p-regular-16 py-4">
+            Ready to add your product? It only takes a few minutes
+          </p>
+        </div>
       </section>
       <div className="wrapper my-8">
         <ProductListingsForm userId={userId} type="Create" />
