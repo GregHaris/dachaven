db835b6e591d3768ea0885ddd83e7f15fda577c9
Grëg Häris <d.gregharis@gmail.com>
2025-02-10 20:57:45 +0100

    fet: add check for if product is available

--- components/shared/CheckoutButton.tsx
+++ components/shared/CheckoutButton.tsx
@@ -5,7 +5,6 @@ import { useState, useEffect } from 'react';
 
 import { Button } from '../ui/button';
 import { IProductListing } from '@/lib/database/models/productListing.model';
-import { hasUserPurchasedProductListings } from '@/lib/actions/order.actions';
 import Checkout from './Checkout';
 
 const CheckoutButton = ({
@@ -14,22 +13,6 @@ const CheckoutButton = ({
   productListing: IProductListing;
 }) => {
   const { isLoaded, user } = useUser();
-  const [hasPurchased, setHasPurchased] = useState(false);
-
-  useEffect(() => {
-    const checkPurchase = async () => {
-      if (user) {
-        const userId = user.publicMetadata.userId as string;
-        const purchased = await hasUserPurchasedProductListings(
-          userId,
-          productListing._id
-        );
-        setHasPurchased(purchased ?? false);
-      }
-    };
-
-    checkPurchase();
-  }, [user, productListing._id]);
 
   if (!isLoaded) {
     return <div>Loading...</div>;
@@ -38,18 +21,16 @@ const CheckoutButton = ({
   const userId = user?.publicMetadata.userId as string;
   const isProductListingsCreator =
     userId === productListing.seller._id.toString();
-  const hasProductListingsFinished =
-    new Date(productListing.endDateTime) < new Date();
+
+  const isProductAvailable = productListing.isAvailable === false;
 
   return (
     !isProductListingsCreator && (
       <div className="flex item-center gap-3">
-        {hasProductListingsFinished ? (
+        {!isProductAvailable ? (
           <p className="p-2 text-red-400">
             Sorry, tickets are no longer available.
           </p>
-        ) : hasPurchased ? (
-          <p className="p-2 text-red-400">Ticket already purchased</p>
         ) : (
           <>
             <SignedOut>
@@ -59,7 +40,7 @@ const CheckoutButton = ({
                   size={'lg'}
                   asChild
                 >
-                  Get Tickets
+                  Purchase Item
                 </Button>
               </SignInButton>
             </SignedOut>
