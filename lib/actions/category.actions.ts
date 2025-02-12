c75f9b5bfde6da950d65c02bdfc8264fd24ac9d5
Grëg Häris <d.gregharis@gmail.com>
2025-02-12 05:53:47 +0100

    fix: create the category from the categories constants

---
+++ lib/actions/category.actions.ts
@@ -0,0 +1,49 @@
+'use server';
+
+import { connectToDatabase } from '../database';
+import { CreateCategoryParams } from '@/types';
+import { handleError } from '../utils';
+import Category from '../database/models/category.model';
+import { categories } from '@/constants';
+
+export const createCategory = async ({
+  categoryName,
+}: CreateCategoryParams) => {
+  try {
+    await connectToDatabase();
+    const newCategory = await Category.create({ name: categoryName });
+
+    return JSON.parse(JSON.stringify(newCategory));
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const getAllCategories = async () => {
+  try {
+    await connectToDatabase();
+    const categories = await Category.find();
+
+    return JSON.parse(JSON.stringify(categories));
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const initializeCategories = async () => {
+  try {
+    await connectToDatabase();
+    const existingCategories = await Category.find().select('name');
+    const existingCategoryNames = existingCategories.map((cat) => cat.name);
+
+    const newCategories = categories.filter(
+      (category) => !existingCategoryNames.includes(category)
+    );
+
+    if (newCategories.length > 0) {
+      await Category.insertMany(newCategories.map((name) => ({ name })));
+    }
+  } catch (error) {
+    handleError(error);
+  }
+};
