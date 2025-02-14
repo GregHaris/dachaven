57307c73a30d7f1297971d08b451573ecc1527c5
Grëg Häris <d.gregharis@gmail.com>
2025-02-14 10:35:45 +0100

    feat: add heading texts

--- app/(root)/admin/page.tsx
+++ app/(root)/admin/page.tsx
@@ -12,12 +12,15 @@ export default async function AdminDashboard({
   return (
     <div className="p-6 bg-gray-100 min-h-screen">
       <div className="max-w-4xl mx-auto bg-white p-6 rounded-lg shadow-md">
-        <h1 className="text-2xl font-bold mb-4">Admin Dashboard</h1>
-        <p className="mb-6">
+        <h1 className="text-4xl font-bold text-center mb-4">Admin Dashboard</h1>
+        <p className="mb-6 text-center">
           This is the protected admin dashboard restricted to users with the
           `admin` role.
         </p>
         <div className="mb-6">
+          <h2 className="text-2xl font-bold mb-4">
+            Set Users Role
+          </h2>
           <SearchUsers />
         </div>
         <div className="mb-6">
